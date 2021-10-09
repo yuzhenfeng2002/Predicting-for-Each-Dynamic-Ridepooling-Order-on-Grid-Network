bf3136975d298f9c3acd806cbea13818e7900b8b
Feng-Yz <1150125812@qq.com>
2021-10-09 19:58:09 +0800

    Experiment for B-graph

--- Ridesharing-Matching-Prediction/Ridesharing-Matching-Prediction/error_test_main.cpp
+++ Ridesharing-Matching-Prediction/Ridesharing-Matching-Prediction/error_test_main.cpp
@@ -9,15 +9,15 @@
 #include <ctime>
 #include "Network.hpp"
 
-const int MAX_OD_NUM = 20;
+const int MAX_OD_NUM = 100;
 int main(int argc, const char * argv[]) {
     for (int i = 0; i < MAX_OD_NUM; i++) {
         Network network = Network(180, 180, 1, 5, 5, 1);
-        network.generateODPairs("/Users/fxb/Desktop/误差测试2/OD_Cluster_" + std::to_string(i) + ".csv");
+        network.generateODPairs("/Users/fxb/Desktop/误差测试/OD_Cluster_" + std::to_string(i) + ".csv");
         network.generateVariables();
         int iteration_time = 0;
         iteration_time = network.iteration(0.01, 0.01).first; // iteration
-        network.calPredictionResult("/Users/fxb/Desktop/误差测试2/OD_Cluster_Result_" + std::to_string(i) + ".csv");
+        network.calPredictionResult("/Users/fxb/Desktop/误差测试/OD_Cluster_Result_" + std::to_string(i) + ".csv");
     }
     return 0;
 }
