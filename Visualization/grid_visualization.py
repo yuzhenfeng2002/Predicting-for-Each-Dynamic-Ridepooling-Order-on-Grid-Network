30edb998885be61dd7386f29c3b6a10b7854c024
Feng-Yz <1150125812@qq.com>
2021-10-21 14:44:05 +0800

    Experiment for Spectral Clustering

--- Visualization/grid_visualization.py
+++ Visualization/grid_visualization.py
@@ -2,7 +2,8 @@ import csv
 import matplotlib.pyplot as plt
 
 for i in range(1):
-    with open("RESULT_2000.csv", mode='r') as file:
+    plt.figure()
+    with open("RESULT_1e4.csv", mode='r') as file:
         data = csv.reader(file)
         head: list = next(data)
         id_col = head.index("id")
@@ -17,7 +18,7 @@ for i in range(1):
             x2 = int(row[x2_col])
             y1 = int(row[y1_col])
             y2 = int(row[y2_col])
-            if j <= 2000:
+            if j <= 1e4:
                 # plt.plot([x1, x2, x2], [y1, y1, y2], 'b', linewidth=0.5)
                 plt.plot(x1, y1, 'ro', markersize=0.5)
                 plt.plot(x2, y2, 'ko', markersize=0.5)
